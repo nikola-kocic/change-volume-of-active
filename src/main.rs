4600786fb5f90a34fb2409c63c12e6725c663fcb
Nikola Kocić <nikolakocic@gmail.com>
2016-11-07 20:40:39 +0100

    toggle mute of active process

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,21 @@ extern crate libpulse_sys;
 use xcb_util::ewmh;
 use std::ptr::{null, null_mut};
 use std::ffi::{CStr, CString};
-use self::libc::{c_void, c_int, c_uint};
+use self::libc::{c_char, c_int, c_void};
 
-fn pulse_info() {
+
+fn pulse_info(pid: u32) {
     use libpulse_sys::*;
 
-    // This callback gets called when our context changes state.  We really only
-    // care about when it's ready or if it has failed
+    struct SinkInputInfo {
+        pid: u32,
+        found: bool,
+        info: pa_sink_input_info,
+    }
+
     unsafe extern "C" fn pa_state_cb(pa_ctx: *mut pa_context, userdata: *mut c_void) -> () {
         let pa_state: pa_context_state_t = pa_context_get_state(pa_ctx);
-        let mut pa_ready = userdata as *mut c_uint;
+        let mut pa_ready = userdata as *mut pa_context_state_t;
         *pa_ready = pa_state;
         // println!("Pulse state: {}", pa_state);
     }
@@ -27,15 +32,20 @@ fn pulse_info() {
         if eol > 0 {
             return;
         }
+
         let p = (*i).proplist;
         let pa_prop_application_process_id = CString::new("application.process.id").unwrap().as_ptr();
         if pa_proplist_contains(p, pa_prop_application_process_id) == 1 {
-            let app_pid = pa_proplist_gets(p, pa_prop_application_process_id);
-            let app_name = CStr::from_ptr((*i).name).to_str().unwrap();
-            let pid_s = CStr::from_ptr(app_pid).to_str().unwrap();
-            println!("{} : {}", pid_s, app_name);
-            let pa_userdata = userdata as *mut c_uint;
-            *pa_userdata = (*i).sink;
+            let pid = {
+                let pid_c: *const c_char = pa_proplist_gets(p, pa_prop_application_process_id);
+                let pid_s = CStr::from_ptr(pid_c).to_str().unwrap();
+                pid_s.parse::<u32>().unwrap()
+            };
+            let mut pa_userdata = userdata as *mut SinkInputInfo;
+            if (*pa_userdata).pid == pid {
+                (*pa_userdata).found = true;
+                (*pa_userdata).info = *i;
+            }
         }
     }
 
@@ -46,7 +56,7 @@ fn pulse_info() {
         let pa_mlapi: *mut pa_mainloop_api = pa_mainloop_get_api(pa_ml);
         let client_name = CString::new("test").unwrap();
         let pa_ctx: *mut pa_context = pa_context_new(pa_mlapi, client_name.as_ptr());
-        let mut pa_ready: u32 = 0u32;
+        let mut pa_ready: pa_context_state_t = 0u32;
         let pa_ready_ptr = &mut pa_ready as *mut _ as *mut c_void;
         pa_context_set_state_callback(pa_ctx, Some(pa_state_cb), pa_ready_ptr);
 
@@ -54,6 +64,7 @@ fn pulse_info() {
         pa_context_connect(pa_ctx, null(), 0, null());
         let mut state = 0;
         let mut pa_op: *mut pa_operation = null_mut();
+        let mut pa_userdata = SinkInputInfo { pid: pid, found: false, info: pa_sink_input_info::default() };
         loop {
             match pa_ready {
                 PA_CONTEXT_READY => {
@@ -71,15 +82,33 @@ fn pulse_info() {
                 }
             }
 
+            let pa_userdata_ptr = &mut pa_userdata as *mut _ as *mut c_void;
             match state {
                 0 => {
-                    let mut pa_userdata: u32 = 0u32;
-                    let pa_userdata_ptr = &mut pa_userdata as *mut _ as *mut c_void;
                     pa_op = pa_context_get_sink_input_info_list(pa_ctx, Some(pa_sink_input_info_cb), pa_userdata_ptr);
                     assert!(!pa_op.is_null());
                     state += 1;
                 }
                 1 => {
+                    let op_state: pa_operation_state_t = pa_operation_get_state(pa_op);
+                    if op_state == PA_OPERATION_DONE {
+                        if pa_userdata.found {
+                            println!("muting {}", pa_userdata.info.index);
+                            let mute = if pa_userdata.info.mute == 0 { 1 } else { 0 };
+                            pa_operation_unref(pa_op);
+                            pa_op = pa_context_set_sink_input_mute(pa_ctx, pa_userdata.info.index, mute, None, null_mut());
+                            state += 1;
+                        } else {
+                            // Now we're done, clean up and disconnect and return
+                            pa_operation_unref(pa_op);
+                            pa_context_disconnect(pa_ctx);
+                            pa_context_unref(pa_ctx);
+                            pa_mainloop_free(pa_ml);
+                            break;
+                        }
+                    }
+                }
+                2 => {
                     let op_state: pa_operation_state_t = pa_operation_get_state(pa_op);
                     if op_state == PA_OPERATION_DONE {
                         // Now we're done, clean up and disconnect and return
@@ -91,7 +120,7 @@ fn pulse_info() {
                     }
                 }
                 _ => {
-                    println!("state _");
+                    assert!(false);
                 }
             }
             // Iterate the main loop and go again.  The second argument is whether
@@ -100,7 +129,6 @@ fn pulse_info() {
             pa_mainloop_iterate(pa_ml, 1, null_mut());
         }
     }
-    println!("DONE");
 }
 
 fn active_window_pid() -> u32 {
@@ -115,5 +143,6 @@ fn active_window_pid() -> u32 {
 fn main() {
     let pid = active_window_pid();
     println!("pid: {}", pid);
-    pulse_info();
+    pulse_info(pid);
+    println!("DONE");
 }
