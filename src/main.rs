d5774e325977468fbb105ec048acc77fc1e67628
Nikola Kocić <nikolakocic@gmail.com>
2016-11-07 20:51:52 +0100

    small refactor, to reduce code duplication

--- src/main.rs
+++ src/main.rs
@@ -51,83 +51,81 @@ fn pulse_info(pid: u32) {
 
     // pacmd list-sink-inputs
     unsafe {
+        let client_name = CString::new("test").unwrap();
+        let mut pa_ready: pa_context_state_t = 0u32;
+        let mut state = 0;
+        let mut pa_op: *mut pa_operation = null_mut();
+        let mut pa_userdata = SinkInputInfo {
+            pid: pid, found: false, info: pa_sink_input_info::default()
+        };
+
         // Create a mainloop API and connection to the default server
         let pa_ml: *mut pa_mainloop = pa_mainloop_new();
         let pa_mlapi: *mut pa_mainloop_api = pa_mainloop_get_api(pa_ml);
-        let client_name = CString::new("test").unwrap();
         let pa_ctx: *mut pa_context = pa_context_new(pa_mlapi, client_name.as_ptr());
-        let mut pa_ready: pa_context_state_t = 0u32;
         let pa_ready_ptr = &mut pa_ready as *mut _ as *mut c_void;
         pa_context_set_state_callback(pa_ctx, Some(pa_state_cb), pa_ready_ptr);
-
-        // This function connects to the pulse server
         pa_context_connect(pa_ctx, null(), 0, null());
-        let mut state = 0;
-        let mut pa_op: *mut pa_operation = null_mut();
-        let mut pa_userdata = SinkInputInfo { pid: pid, found: false, info: pa_sink_input_info::default() };
+
         loop {
             match pa_ready {
                 PA_CONTEXT_READY => {
-                }
-                PA_CONTEXT_FAILED | PA_CONTEXT_TERMINATED => {
-                    println!("Failed to connect to PulseAudio!");
-                    pa_context_disconnect(pa_ctx);
-                    pa_context_unref(pa_ctx);
-                    pa_mainloop_free(pa_ml);
-                    break;
-                }
-                _ => {
-                    pa_mainloop_iterate(pa_ml, 1, null_mut());
-                    continue;
-                }
-            }
-
-            let pa_userdata_ptr = &mut pa_userdata as *mut _ as *mut c_void;
-            match state {
-                0 => {
-                    pa_op = pa_context_get_sink_input_info_list(pa_ctx, Some(pa_sink_input_info_cb), pa_userdata_ptr);
-                    assert!(!pa_op.is_null());
-                    state += 1;
-                }
-                1 => {
-                    let op_state: pa_operation_state_t = pa_operation_get_state(pa_op);
-                    if op_state == PA_OPERATION_DONE {
-                        if pa_userdata.found {
-                            println!("muting {}", pa_userdata.info.index);
-                            let mute = if pa_userdata.info.mute == 0 { 1 } else { 0 };
-                            pa_operation_unref(pa_op);
-                            pa_op = pa_context_set_sink_input_mute(pa_ctx, pa_userdata.info.index, mute, None, null_mut());
+                    match state {
+                        0 => {
+                            let pa_userdata_ptr = &mut pa_userdata as *mut _ as *mut c_void;
+                            pa_op = pa_context_get_sink_input_info_list(
+                                pa_ctx, Some(pa_sink_input_info_cb), pa_userdata_ptr);
+                            assert!(!pa_op.is_null());
                             state += 1;
-                        } else {
-                            // Now we're done, clean up and disconnect and return
-                            pa_operation_unref(pa_op);
-                            pa_context_disconnect(pa_ctx);
-                            pa_context_unref(pa_ctx);
-                            pa_mainloop_free(pa_ml);
-                            break;
+                        }
+                        1 => {
+                            let op_state: pa_operation_state_t = pa_operation_get_state(pa_op);
+                            if op_state == PA_OPERATION_DONE {
+                                if !pa_userdata.found {
+                                    break;
+                                } else {
+                                    let mute = if pa_userdata.info.mute == 0 { 1 } else { 0 };
+                                    println!("setting mute to {} for {}", mute, pa_userdata.info.index);
+                                    pa_operation_unref(pa_op);
+                                    pa_op = pa_context_set_sink_input_mute(
+                                        pa_ctx, pa_userdata.info.index, mute, None, null_mut());
+                                    assert!(!pa_op.is_null());
+                                    state += 1;
+                                }
+                            }
+                        }
+                        2 => {
+                            let op_state: pa_operation_state_t = pa_operation_get_state(pa_op);
+                            if op_state == PA_OPERATION_DONE {
+                                break;
+                            }
+                        }
+                        _ => {
+                            assert!(false);
                         }
                     }
                 }
-                2 => {
-                    let op_state: pa_operation_state_t = pa_operation_get_state(pa_op);
-                    if op_state == PA_OPERATION_DONE {
-                        // Now we're done, clean up and disconnect and return
-                        pa_operation_unref(pa_op);
-                        pa_context_disconnect(pa_ctx);
-                        pa_context_unref(pa_ctx);
-                        pa_mainloop_free(pa_ml);
-                        break;
-                    }
+                PA_CONTEXT_FAILED | PA_CONTEXT_TERMINATED => {
+                    println!("Failed to connect to PulseAudio!");
+                    break;
                 }
                 _ => {
-                    assert!(false);
                 }
             }
+
             // Iterate the main loop and go again.  The second argument is whether
             // or not the iteration should block until something is ready to be
             // done.  Set it to zero for non-blocking.
             pa_mainloop_iterate(pa_ml, 1, null_mut());
         }
+
+        // Cleanup
+        if !pa_op.is_null() {
+            pa_operation_unref(pa_op);
+        }
+        pa_context_disconnect(pa_ctx);
+        pa_context_unref(pa_ctx);
+        pa_mainloop_free(pa_ml);
     }
 }
 
@@ -144,5 +142,4 @@ fn main() {
     let pid = active_window_pid();
     println!("pid: {}", pid);
     pulse_info(pid);
-    println!("DONE");
 }
