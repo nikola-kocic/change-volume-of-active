663658d6f0d81d707d8c6a176d663e584af0ef70
Nikola Kocic <nikolakocic@gmail.com>
2017-06-29 17:09:07 +0200

    Fix debug log

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,12 @@ mod pulseop;
 
 fn main() {
     let arguments = args::get_arguments();
-    if arguments.debug {
-        println!("op = {:?}", arguments.operation);
-        println!("pid: {:?}", arguments.pid);
-    }
     let pid: u32 = arguments.pid.unwrap_or_else(|| {
         activewindow::active_window_pid(arguments.debug)
     });
+    if arguments.debug {
+        println!("op = {:?}", arguments.operation);
+        println!("pid: {:?}", pid);
+    }
     pulseop::pulse_op(pid, &arguments.operation, arguments.debug);
 }
